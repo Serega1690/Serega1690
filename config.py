82cf028553ab8ac084cd5af7e52dde75718aa33d
Serega1690 <120504392+Serega1690@users.noreply.github.com>
2023-01-09 08:41:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 user_token = '' # токен пользователя
-comm_token = 'vk1.a.pK4Yn3p2SiigslgbHbaVAv6AT3j7jS2jx9DSVoxKklrBHdh4ZkcyDE9nsVjxr3ViPIP3K3zB0jqwfvqc3ij8Rl9pWHZtNrUTmdhvlI3ah0L1PBLouSeHdBJmgvgnu1028HBbFf-fzGInkzaDGUjoJQ_mapa2rEMTedK08W7s2-RfHRl3VsfybcZ97vDLDdu6W2MenYjWCr1eXyLntWxing' # токен сообщества
+comm_token = '' # токен сообщества
 
 vkinder_db_user = 'vkinder'
 vkinder_db_pass = '123456'
